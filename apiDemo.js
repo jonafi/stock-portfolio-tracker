aeda75f2dabeb6b984052061d5986402dcc4a60a
John Leintz <johnleintz@scaledflow.com>
2020-07-08 19:15:12 -0500

    updated apiDemo.js

---
+++ apiDemo.js
@@ -0,0 +1,65 @@
+
+// NPM crypto-price
+const price = require('crypto-price')
+
+// NPM finnhub
+const finnhub = require("finnhub");
+const request = require("request");
+
+const api_key = finnhub.ApiClient.instance.authentications["api_key"];
+api_key.apiKey = "brvkn6nrh5rd378r3l5g"; // Replace this
+const finnhubClient = new finnhub.DefaultApi();
+
+// var covid = [];
+
+///////////////////////////////////////////////////
+// This section is for fix header 
+///////////////////////////////////////////////////
+
+
+// STOCK PRICES
+finnhubClient.quote("GLD", (error, data) => {
+  // CURRENT PRICE
+  console.log("GLD = " + data.c);
+});
+
+
+// BITCOIN PRICE
+// https://www.npmjs.com/package/crypto-price
+price.getCryptoPrice("USD", "BTC").then(obj => { // Base for ex - USD, Crypto for ex - ETH 
+    console.log("BIT COIN = " + obj.price)
+}).catch(err => {
+    console.log(err)
+})
+ 
+///////////////////////////////////////////////////
+// This section is for the body
+///////////////////////////////////////////////////
+
+// STOCK TICKER & COMPANY NAME
+request('https://finnhub.io/api/v1/stock/profile2?symbol=AMZN&token=brvkn6nrh5rd378r3l5g', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  console.log("STOCK TICKER = " + body.ticker);
+  console.log("COMPANY NAME = " + body.name);
+});
+
+// STOCK PRICES
+finnhubClient.quote("AMZN", (error, data) => {
+  // CURRENT PRICE
+  console.log("CURRENT PRICE = " + data.c);
+  // HIGH DAY PRICE
+  console.log("HIGH = " + data.h);
+  // LOW DAY PRICE
+  console.log("LOW = " + data.l);
+});
+
+// PRICE TARGETS
+request('https://finnhub.io/api/v1/stock/price-target?symbol=AMZN&token=brvkn6nrh5rd378r3l5g', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  // TARGET HIGH
+  console.log("HIGH TG = " + body.targetHigh);
+  // TARGET LOW
+  console.log("LOW TG = " + body.targetLow);
+  console.log("Mediam TG = " + body.targetMedian);
+});
+
