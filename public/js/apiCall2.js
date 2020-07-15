48be63196cc838ad81bc8c43eef75938460046f1
Davis Sjoberg <59490187+iron318davis@users.noreply.github.com>
2020-07-14 21:31:09 -0500

    Almost MVP

--- public/js/apiCall2.js
+++ public/js/apiCall2.js
@@ -1,13 +1,29 @@
 $(document).ready(() => {
   // called the /finntest from api-routes.js
-  $.get("/finntest").then(finnresponse => {
-    // quote1 is an ID for HTML to use, used currently in index.handlebars
-    $("#quote1").text(JSON.stringify(finnresponse.data));
-    console.log("Hit finntest get and " + finnresponse.data);
-    console.log("Hit finntest get and data.o =" + finnresponse.data.o);
-    console.log("Hit finntest get and data.c =" + finnresponse.data.c);
-    console.log("Hit finntest get and data.h =" + finnresponse.data.h);
-  });
+  const stocks = ["GLD", "AMZN"];
+
+  for (i = 0; i < stocks.length; i++) {
+    $.get(`finntest/${stocks[i]}`).then(finnresponse => {
+      // quote1 is an ID for HTML to use, used currently in index.handlebars
+      $("#quote1").text(JSON.stringify(finnresponse.data));
+      const variablefinnres = JSON.stringify(finnresponse);
+      console.log(variablefinnres);
+      console.log("Hit finntest get and " + finnresponse.datatest);
+      console.log("Hit finntest get and data.o =" + finnresponse.datatest.o);
+      console.log("Hit finntest get and data.c =" + finnresponse.datatest.c);
+      console.log("Hit finntest get and data.h =" + finnresponse.datatest.h);
+    });
+  }
+//   $.get("/finntest/GLD").then(finnresponse => {
+//     // quote1 is an ID for HTML to use, used currently in index.handlebars
+//     $("#quote1").text(JSON.stringify(finnresponse.data));
+//     const variablefinnres = JSON.stringify(finnresponse.data);
+//     console.log(variablefinnres);
+//     console.log("Hit finntest get and " + finnresponse.data);
+//     console.log("Hit finntest get and data.o =" + finnresponse.data.o);
+//     console.log("Hit finntest get and data.c =" + finnresponse.data.c);
+//     console.log("Hit finntest get and data.h =" + finnresponse.data.h);
+//   });
 
   $("#addStockButton").on("click", () => {
     event.preventDefault();
@@ -20,11 +36,11 @@ $(document).ready(() => {
     console.log(portfolioValue);
     //   $.get("/api/updateportfolio", {portfolio_name: portfolioValue, stock_2: tickerValue});
     $.ajax({
-        type: "PUT",
-        url: "/api/updateportfolio",
-        data: {portfolioValue, tickerValue},
-        success: (res) => console.log(res),
-      });
+      type: "PUT",
+      url: "/api/updateportfolio",
+      data: { portfolioValue, tickerValue },
+      success: res => console.log(res)
+    });
     // $.get("/api/updateportfolio", function(portfolioValue) {
     //     console.log("trying to get portfolioValue to show up somewhere " + portfolioValue);
 
@@ -33,7 +49,6 @@ $(document).ready(() => {
     //     // res.redirect(307, "/api/login");
     //     console.log("Done SQLing " + portfolioValue);
     //   })
-    ;
   });
   //   Next step is pass tickerValue and portfolioValue to API so it will return quote info
 });
