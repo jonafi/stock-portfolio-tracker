155077b797956947e4f3ff6fd39dc665108394cd
J Ferro <jonafi@gmail.com>
2020-07-15 11:45:01 -0500

    added portfolio starter seed sql

---
+++ database/portfolio-seed.sql
@@ -0,0 +1,17 @@
+INSERT INTO Portfolios (portfolio_name, display, stock_1, createdAt, updatedAt)
+VALUES
+    ('Energy', 1, 'BP', 0, 0),
+    ('Materials', 1, 'EXP', 0, 0),
+	('Industrials', 1, 'JCI', 0, 0),
+	('Consumer Discretionary', 1 , 'HBI', 0, 0),
+    ('Consumer Staples', 1, 'CL', 0, 0),
+    ('Health Care', 1 ,'JNJ', 0, 0),
+	('Financials', 1, 'BAC', 0, 0),
+	('Information Technology', 1, 'IBM', 0, 0),
+	('Telecommunication Services', 1, 'AMT', 0, 0),
+	('Utilities', 1, 'OGE', 0, 0),
+	('Real Estate', 1, 'BBX', 0, 0),
+	('Custom One', 1, 'MMM', 0, 0),
+	('Custom Two', 1, 'T', 0, 0),
+    ('Custom Three', 1, 'HD', 0, 0)
+    ;
